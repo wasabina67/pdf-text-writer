c52556f2fba86e141a356cdf1072e5f34ac4eef1
wasabina67 <wasabina67@gmail.com>
2025-10-05 22:05:45 +0900

    Update config.py, pdf/sample.pdf

--- config.py
+++ config.py
@@ -1,7 +1,52 @@
 page_data = [
     {
-        "x_position": 1,
-        "y_position": 1,
-        "text": "1234",
+        "x_position": 90,
+        "y_position": 712,
+        "text": "1行目のテキスト",
+    },
+    {
+        "x_position": 90,
+        "y_position": 673,
+        "text": "2行目のテキスト",
+    },
+    {
+        "x_position": 90,
+        "y_position": 634,
+        "text": "3行目のテキスト",
+    },
+    {
+        "x_position": 90,
+        "y_position": 595,
+        "text": "4行目のテキスト",
+    },
+    {
+        "x_position": 90,
+        "y_position": 556,
+        "text": "5行目のテキスト",
+    },
+    {
+        "x_position": 90,
+        "y_position": 517,
+        "text": "6行目のテキスト",
+    },
+    {
+        "x_position": 90,
+        "y_position": 478,
+        "text": "7行目のテキスト",
+    },
+    {
+        "x_position": 90,
+        "y_position": 439,
+        "text": "8行目のテキスト",
+    },
+    {
+        "x_position": 90,
+        "y_position": 400,
+        "text": "9行目のテキスト",
+    },
+    {
+        "x_position": 90,
+        "y_position": 361,
+        "text": "10行目のテキスト",
     },
 ]
